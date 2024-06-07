9696ab39056cfdd3e4f851412655b66f325b524b
Svetlin <167436978+zensii@users.noreply.github.com>
2024-06-07 22:31:52 +0300

    created the LIST project

--- LIST_methods_system/list_methods_system.py
+++ LIST_methods_system/list_methods_system.py
@@ -14,87 +14,155 @@ def display_menu():
     print("12. Exit")
 
 
+
 def handle_append(lst):
-    # TODO: Prompt the user for a value to append to the list
-    # Use the append() method to add the value to the list
-    # Print the updated list
-    pass
+    """
+    The append method is used to add item/s to the end of the list.
+    """
+    print()
+    object_to_append = input('Please enter what you want to append to the list: ')
+    lst.append(object_to_append)
+    return lst
 
 
 def handle_extend(lst):
-    # TODO: Prompt the user for values to extend the list (comma-separated)
-    # Use the extend() method to add these values to the list
-    # Print the updated list
-    pass
+    """
+    The extend() method is used to add item/s from an iterable to the end of the list.
+    """
+    print()
+    object_to_extend = input('Please enter with what you want to extend the list with(comma-separated): ')
+
+    # strips to remove a whitespace after the comma
+    stripped_object = []
+    for item in object_to_extend.split(','):
+        stripped_object.append(item.strip())
+
+    lst.extend(stripped_object)
+
+    return lst
 
 
 def handle_insert(lst):
-    # TODO: Prompt the user for an index and a value to insert at that index
-    # Use the insert() method to add the value at the specified index
-    # Print the updated list
-    pass
+
+    help_method = 'Use the insert() method to add the value at the specified index'
+    print()
+    object_to_insert = input('Please enter the object to insert :')
+    index = input('Index to insert at: ')
+
+    lst.insert(int(index), object_to_insert)
+    return lst
 
 
 def handle_remove(lst):
-    # TODO: Prompt the user for a value to remove from the list
-    # Use the remove() method to delete the first occurrence of the value
-    # Handle the case where the value is not found in the list
-    # Print the updated list
-    pass
+
+    help_method = 'remove method removes the first occurrence of the given value'
+    print()
+    object_to_remove = input('Please enter the object you want to remove the first occurrence of: ')
+
+    try:
+        lst.remove(object_to_remove)
+        return lst
+    except ValueError:
+        print(f'{object_to_remove} not found in the list')
+        return lst
 
 
 def handle_pop(lst):
-    # TODO: Prompt the user for an index to pop (optional, leave empty to pop last item)
-    # Use the pop() method to remove the item at the specified index or the last item if no index is provided
-    # Handle the case where the index is out of range
-    # Print the updated list
-    pass
 
+    help_method = 'Use the pop() method to remove the item at the specified index or the last item if no index is provided'
+
+    index_to_remove_item_from = input('Please specify index to pop or leave blank for last object: ')
+
+    if index_to_remove_item_from == '':
+        lst.pop()
+        return lst
+
+    try:
+        lst.pop(int(index_to_remove_item_from))
+
+    except ValueError:
+        print(f'UNSUCCESSFUL! You should choose an index from 0 to {len(lst)-1} or leave blank for last object.')
+
+    return lst
 
 def handle_clear(lst):
-    # TODO: Use the clear() method to remove all items from the list
-    # Print the updated list
-    pass
+
+    help_method = 'Use the clear() method to remove all items from the list'
+
+    confirm = input('Are you sure you want to clear the list?(type "YES" to confirm): ')
+
+    if confirm == 'YES':
+        lst.clear()
+        return lst
+    else:
+        print('Clearing the list aborted.')
+    return lst
 
 
 def handle_index(lst):
-    # TODO: Prompt the user for a value to find its index
-    # Use the index() method to find the index of the value
-    # Handle the case where the value is not found in the list
-    # Print the index of the value
-    pass
+
+    help_method = 'Use the index() method to find the index of a given object'
+
+    value_to_search = input('Please enter a value to search the index of: ')
+
+    try:
+        print()
+        print(f'{value_to_search} can be found at index {lst.index(value_to_search)}.')
+    except ValueError:
+        print()
+        print(f'{value_to_search} not found in the list.')
+
+    return lst
 
 
 def handle_count(lst):
-    # TODO: Prompt the user for a value to count its occurrences in the list
-    # Use the count() method to count how many times the value appears in the list
-    # Print the count of the value
-    pass
+
+    help_method = 'counts how many times the given object appears in the list'
+
+    object_to_search_for = input('Please input the object to count the occurrence of: ')
+
+    print(f'{object_to_search_for} can be found {lst.count(object_to_search_for)} times in the list')
+
+    return lst
 
 
 def handle_sort(lst):
-    # TODO: Use the sort() method to sort the list in ascending order
-    # Print the updated list
-    pass
+
+    help_method = 'The sort() method sorts the list in ascending order'
+
+    lst.sort()
+    print()
+    print(f'Sorting the list...')
+
+    return lst
 
 
 def handle_reverse(lst):
-    # TODO: Use the reverse() method to reverse the order of the list
-    # Print the updated list
-    pass
+
+    help_method = 'reverse creates a reverse object of the give one'
+
+    print(f'Reversing the list...')
+
+    return lst
 
 
 def handle_copy(lst):
-    # TODO: Use the copy() method to create a shallow copy of the list
-    # Print the copied list
-    pass
+
+    help_method = 'copy returns a shallow copy of the list'
+
+    print(f'This is a shallow copy of the current state of the list: {lst.copy()}')
+
+    return lst
 
 
 def main():
+
     initial_values = input("Enter initial list values (comma-separated): ")
     lst = initial_values.split(',')
 
     while True:
+        print()
+        print(f'The current state of the list is: {lst}')
         display_menu()
         choice = input("Enter your choice (1-12): ")
         if choice == '1':
